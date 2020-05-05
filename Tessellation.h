5d12ea603d812bbf967015186cbbf0aafb38513d
Patrick Owen <patowen95@gmail.com>
2020-05-04 20:15:24 -0400

    Fix bug with octahedron construction

--- Tessellation.h
+++ Tessellation.h
@@ -8,19 +8,27 @@ class Tessellation {
 public:
 	void testTessellation() {
 		createSeedFace();
-		for (unsigned i=0; i<5; ++i) {
+		for (unsigned i=0; i<3; ++i) {
 			std::cout << faces.size() << ", " << vertices.size() << "\n";
 			size_t currentCount = faces.size();
+			std::cout << "Current data: \n";
+			for (size_t j=0; j<currentCount; ++j) {
+				inspectFace(FaceRef(j));
+			}
+
+			std::cout << "\nNew data: \n";
 			for (size_t j=0; j<currentCount; ++j) {
 				for (unsigned k=0; k<n; ++k) {
 					if (faces[j].adjacentFaces[k].index == FaceRef::nullRef()) {
-						createAdjacentFace(FaceRef(j), k);
+						FaceRef face = createAdjacentFace(FaceRef(j), k);
+						inspectFace(face);
 					}
 				}
 			}
 		}
 
 		std::cout << faces.size() << ", " << vertices.size() << "\n";
+		std::cout << "Done\n";
 	}
 
 private:
@@ -41,6 +49,10 @@ private:
 		FaceRef(): index(nullRef()) {}
 		FaceRef(size_t index): index(index) {}
 		size_t index;
+
+		bool isNull() {
+			return index == nullRef();
+		}
 	};
 
 	class VertexRef {
@@ -52,14 +64,16 @@ private:
 		VertexRef(): index(nullRef()) {}
 		VertexRef(size_t index): index(index) {}
 		size_t index;
+
+		bool isNull() {
+			return index == nullRef();
+		}
 	};
 
 	class Face {
 	public:
-		Face() {
-			adjacentFaces.fill(nullIndex());
-		}
-
+		Face(int orientation): orientation(orientation) {}
+		int orientation;
 		std::array<FaceRef, n> adjacentFaces;
 		std::array<VertexRef, n> adjacentVertices;
 	};
@@ -68,18 +82,26 @@ private:
 	public:
 		Vertex(unsigned type, unsigned numAdjacentFaces, FaceRef startFace):
 			type(type),
-			adjacentFaces(numAdjacentFaces, nullIndex()),
+			adjacentFaces(numAdjacentFaces),
 			freeSlotMin(0),
 			freeSlotMax(numAdjacentFaces - 1) {
 			adjacentFaces[numAdjacentFaces - 1] = startFace;
 		}
 
 		void addUpperFace(FaceRef face) {
+			if (isSaturated()) {
+				throw std::runtime_error("Tried to add to a saturated vertex");
+			}
+
 			adjacentFaces[freeSlotMax - 1] = face;
 			freeSlotMax--;
 		}
 
 		void addLowerFace(FaceRef face) {
+			if (isSaturated()) {
+				throw std::runtime_error("Tried to add to a saturated vertex");
+			}
+
 			adjacentFaces[freeSlotMin] = face;
 			freeSlotMin++;
 		}
@@ -111,17 +133,32 @@ private:
 		return vertices[vertex.index];
 	}
 
-	static constexpr size_t nullIndex() {
-		return std::numeric_limits<size_t>::max();
+	void inspectFace(FaceRef faceRef) {
+		Face& face = getFace(faceRef);
+
+		std::cout << "Face " << faceRef.index << ":\n";
+		std::cout << "  adjacent faces: ";
+		for (FaceRef adj : face.adjacentFaces) {
+			std::cout << (adj.isNull() ? "n" : std::to_string(adj.index)) << ", ";
+		}
+		std::cout << "\n  adjacent vertices: ";
+		for (VertexRef adj : face.adjacentVertices) {
+			if (adj.isNull()) {
+				std::cout << "n, ";
+			} else {
+				std::cout << adj.index << "(" << (getVertex(adj).freeSlotMax - getVertex(adj).freeSlotMin) << "), ";
+			}
+		}
+		std::cout << "\n";
 	}
 
 	FaceRef createSeedFace() {
-		faces.emplace_back();
-		FaceRef newFace(faces.size());
+		faces.emplace_back(1);
+		FaceRef newFace(faces.size() - 1);
 
 		for (unsigned newVertexIndex = 0; newVertexIndex != n; ++newVertexIndex) {
 			vertices.emplace_back(newVertexIndex, shape[newVertexIndex] * 2, newFace);
-			VertexRef newVertex(vertices.size());
+			VertexRef newVertex(vertices.size() - 1);
 			getFace(newFace).adjacentVertices[newVertexIndex] = newVertex;
 		}
 
@@ -132,8 +169,14 @@ private:
 		unsigned lowerVertexIndex = (edge + 1u) % n;
 		unsigned upperVertexIndex = (edge + 2u) % n;
 
-		faces.emplace_back();
-		FaceRef newFace(faces.size());
+		int orientation = -getFace(face).orientation;
+
+		if (orientation != 1 && orientation != -1) {
+			throw std::runtime_error("Bad orientation");
+		}
+
+		faces.emplace_back(orientation);
+		FaceRef newFace(faces.size() - 1);
 
 		// Sort out all direct adjacencies
 		// Face to face
@@ -147,8 +190,17 @@ private:
 		getFace(newFace).adjacentVertices[upperVertexIndex] = upperVertex;
 
 		// Vertex to face
-		getVertex(lowerVertex).addUpperFace(newFace);
-		getVertex(upperVertex).addLowerFace(newFace);
+		if (orientation == 1) {
+			getVertex(lowerVertex).addUpperFace(newFace);
+			getVertex(upperVertex).addLowerFace(newFace);
+		} else {
+			getVertex(lowerVertex).addLowerFace(newFace);
+			getVertex(upperVertex).addUpperFace(newFace);
+		}
+
+		if (newFace.index == 7) {
+			std::cout << "Final stretch\n";
+		}
 
 		// Vertex saturation prep
 		unsigned descendingVertexIndex = lowerVertexIndex;
@@ -163,14 +215,21 @@ private:
 			if (!getVertex(descendingVertex).isSaturated()) {
 				break;
 			}
-			FaceRef existingFace = getVertex(descendingVertex).getLowerFace();
+			FaceRef existingFace = orientation == 1 ? getVertex(descendingVertex).getLowerFace() : getVertex(descendingVertex).getUpperFace();
 			descendingEdge = (descendingEdge + n - 1u) % n;
 			descendingVertexIndex = (descendingVertexIndex + n - 1u) % n;
 			descendingVertex = getFace(existingFace).adjacentVertices[descendingVertexIndex];
 
 			getFace(newFace).adjacentFaces[descendingEdge] = existingFace;
+			getFace(existingFace).adjacentFaces[descendingEdge] = newFace;
 			getFace(newFace).adjacentVertices[descendingVertexIndex] = descendingVertex;
-			getVertex(descendingVertex).addUpperFace(newFace);
+			if (descendingVertexIndex != ascendingVertexIndex) {
+				if (orientation == 1) {
+					getVertex(descendingVertex).addUpperFace(newFace);
+				} else {
+					getVertex(descendingVertex).addLowerFace(newFace);
+				}
+			}
 		}
 
 		// Upper vertex saturation
@@ -178,21 +237,28 @@ private:
 			if (!getVertex(ascendingVertex).isSaturated()) {
 				break;
 			}
-			FaceRef existingFace = getVertex(ascendingVertex).getUpperFace();
+			FaceRef existingFace = orientation == 1 ? getVertex(ascendingVertex).getUpperFace() : getVertex(ascendingVertex).getLowerFace();
 			ascendingEdge = (ascendingEdge + 1u) % n;
 			ascendingVertexIndex = (ascendingVertexIndex + 1u) % n;
 			ascendingVertex = getFace(existingFace).adjacentVertices[ascendingVertexIndex];
 
 			getFace(newFace).adjacentFaces[ascendingEdge] = existingFace;
+			getFace(existingFace).adjacentFaces[ascendingEdge] = newFace;
 			getFace(newFace).adjacentVertices[ascendingVertexIndex] = ascendingVertex;
-			getVertex(ascendingVertex).addLowerFace(newFace);
+			if (ascendingVertexIndex != descendingVertexIndex) {
+				if (orientation == 1) {
+					getVertex(ascendingVertex).addLowerFace(newFace);
+				} else {
+					getVertex(ascendingVertex).addUpperFace(newFace);
+				}
+			}
 		}
 
 		// Freshly-created vertices
 		if (ascendingVertexIndex != descendingVertexIndex) {
-			for (unsigned newVertexIndex = ascendingVertexIndex + 1u; newVertexIndex != descendingVertexIndex; newVertexIndex = (newVertexIndex + 1u) % n) {
+			for (unsigned newVertexIndex = (ascendingVertexIndex + 1u) % n; newVertexIndex != descendingVertexIndex; newVertexIndex = (newVertexIndex + 1u) % n) {
 				vertices.emplace_back(newVertexIndex, shape[newVertexIndex] * 2, newFace);
-				VertexRef newVertex(vertices.size());
+				VertexRef newVertex(vertices.size() - 1);
 				getFace(newFace).adjacentVertices[newVertexIndex] = newVertex;
 			}
 		}
